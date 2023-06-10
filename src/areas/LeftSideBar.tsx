86cce04967dc6ca3374424cc7371efc10064627c
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-06-10 23:52:14 +0200

    Visual changes

--- src/areas/LeftSideBar.tsx
+++ src/areas/LeftSideBar.tsx
@@ -1,12 +1,23 @@
 import "./LeftSideBar.scss";
+import { useState } from "react";
+import { RiArrowRightSLine } from "react-icons/ri";
 import Profile from "../components/Profile";
 import TopButtons from "../components/TopButtons";
 import Card from "../components/Card";
 import Balance from "../components/Balance";
 
 function LeftSideBar() {
+  const [openSideBar, setOpenSideBar] = useState(false);
+
   return (
-    <div className="left-side-bar-wrapper">
+    <div
+      className={
+        openSideBar ? "left-side-bar-wrapper active" : "left-side-bar-wrapper"
+      }
+    >
+      <div className="toggle" onClick={() => setOpenSideBar(!openSideBar)}>
+        <RiArrowRightSLine />
+      </div>
       <TopButtons />
       <Profile />
       <Balance />
--- src/components/PlusButton.tsx
+++ src/components/PlusButton.tsx
@@ -1,16 +1,11 @@
 import "./PlusButton.scss";
-import { motion } from "framer-motion";
 import { AiOutlinePlus } from "react-icons/ai";
 
 function PlusButton() {
   return (
-    <motion.button
-      className="plus-btn"
-      whileHover={{ scale: 1.15 }}
-      whileTap={{ scale: 0.9, backgroundColor: "#f492a0" }}
-    >
+    <button className="plus-btn">
       <AiOutlinePlus />
-    </motion.button>
+    </button>
   );
 }
 export default PlusButton;
